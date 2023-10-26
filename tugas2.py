1aa18ac3e2e1302579fcaebbcb0e71738d47488d
Lia Auliya <146051085+liaauliyasi01@users.noreply.github.com>
2023-10-26 14:12:53 +0700

    Tugas DDP Pekan 2
    Lia Auliya_SI01_0110123275

---
+++ tugas2.py
@@ -0,0 +1,36 @@
+# soal no.1
+print("==============================")
+print("Nama\t: Lia Auliya")
+print("NIM\t: 0110123275")
+print("Kelas\t: Sistem Informasi 01")
+print("Alamat\t: Jakarta Selatan")
+print("==============================")
+
+# soal no.2
+print("==============================")
+print("Nama\t: Siti Sadiyah")
+print("NIM\t: 0110123108")
+print("Kelas\t: Sistem Informasi 01")
+print("Alamat\t: Bogor")
+print("==============================")
+
+
+# mendeklarasikan variabel
+bilangan1 = 8
+bilangan2 = 4
+
+# soal no.3
+# menjumlahkan 2 buah bilangan
+print("penjumlahan 2 buah bilangan: ", bilangan1 + bilangan2)
+
+# soal no.4
+# pengurangan 2 buah bilangan
+print("pengurangan 2 buah bilangan: ", bilangan1 - bilangan2)
+
+# soal no.5
+# perkalian 2 buah bilangan
+print("perkalian 2 buah bilangan: ", bilangan1 * bilangan2)
+
+# soal no.6
+# pembagian 2 buah bilangan
+print("pembagian 2 buah bilangan: ", bilangan1 / bilangan2)
